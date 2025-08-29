43d01ee659fb207226bf17922cf887bd3040a278
Sathish g <srisrisrisathish176@gmail.com>
2025-08-29 18:26:10 +0530

    removed products fetching on ui

--- lib/features/bottom nav/view model/bottom_view_model.dart
+++ lib/features/bottom nav/view model/bottom_view_model.dart
@@ -1,3 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final bottomNavigationBarIndex = StateProvider<int>((ref) => 3);
+final bottomNavigationBarIndex = StateProvider<int>((ref) => 0);
