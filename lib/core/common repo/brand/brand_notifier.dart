9c987fadee588ef6d6c9c682f36bc040777e9864
Sathish g <srisrisrisathish176@gmail.com>
2025-08-29 13:58:34 +0530

    updated brand notifier

--- lib/core/common repo/brand/brand_notifier.dart
+++ lib/core/common repo/brand/brand_notifier.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sadhana_cart/core/common%20model/brand/brand_model.dart';
+import 'package:sadhana_cart/core/common%20model/product/product_model.dart';
+import 'package:sadhana_cart/core/common%20repo/product/product_notifier.dart';
 import 'package:sadhana_cart/core/common%20services/brand/brand_service.dart';
 import 'package:sadhana_cart/core/helper/connection_helper.dart';
 import 'package:sadhana_cart/core/helper/hive_helper.dart';
 
+final brandProvider = StateNotifierProvider<BrandNotifier, List<BrandModel>>(
+  (ref) => BrandNotifier(ref)..initialize(),
+);
+
 class BrandNotifier extends StateNotifier<List<BrandModel>> {
-  BrandNotifier() : super([]);
+  final Ref ref;
+  BrandNotifier(this.ref) : super([]);
 
   void initialize() async {
     final bool isInternet = await ConnectionHelper.checkInternetConnection();
@@ -14,5 +21,13 @@ class BrandNotifier extends StateNotifier<List<BrandModel>> {
     } else {
       state = HiveHelper.getBrands();
     }
+  }
+
+  List<ProductModel> getBrandProduct() {
+    final product = ref.watch(productProvider);
+    for (final brand in state) {
+      return product.where((e) => e.brand == brand.name).toList();
+    }
+    return [];
   }
 }
