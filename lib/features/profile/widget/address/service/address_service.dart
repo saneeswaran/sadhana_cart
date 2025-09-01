68b1b9c6927c839d9674eb6c507fa4e92c8f6d94
Sathish g <srisrisrisathish176@gmail.com>
2025-09-01 15:50:04 +0530

    user address notifier finished

--- lib/features/profile/widget/address/service/address_service.dart
+++ lib/features/profile/widget/address/service/address_service.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:sadhana_cart/core/helper/hive_helper.dart';
+import 'package:sadhana_cart/features/profile/widget/address/model/address_model.dart';
 
 class AddressService {
   static const String addressCollection = "address";
@@ -9,4 +13,59 @@ class AddressService {
       .collection(userCollection)
       .doc(currentUserId)
       .collection(addressCollection);
+
+  static Future<void> addAddress({required AddressModel address}) async {
+    try {
+      final docRef = addressRef.doc();
+      final AddressModel addressModel = AddressModel(
+        id: docRef.id,
+        name: address.name,
+        streetName: address.streetName,
+        city: address.city,
+        state: address.state,
+        pinCode: address.pinCode,
+        phoneNumber: address.phoneNumber,
+        timestamp: Timestamp.now(),
+      );
+      await docRef.set(addressModel.toMap());
+    } catch (e) {
+      log("address service error $e");
+    }
+  }
+
+  static Future<List<AddressModel>> fetchAllAddress() async {
+    try {
+      final QuerySnapshot querySnapshot = await addressRef
+          .doc(currentUserId)
+          .collection(addressCollection)
+          .get();
+      if (querySnapshot.docs.isNotEmpty) {
+        final data = querySnapshot.docs
+            .map((e) => AddressModel.fromMap(e.data() as Map<String, dynamic>))
+            .toList();
+        for (final address in data) {
+          await HiveHelper.addAddress(address: address);
+        }
+        return data;
+      }
+    } catch (e) {
+      log("address service error $e");
+    }
+    return [];
+  }
+
+  static Future<bool> deleteAddress({required String id}) async {
+    try {
+      final DocumentSnapshot documentSnapshot = await addressRef.doc(id).get();
+
+      if (documentSnapshot.exists) {
+        await documentSnapshot.reference.delete();
+        return true;
+      }
+    } catch (e) {
+      log("address service error $e");
+      return false;
+    }
+    return false;
+  }
 }
--- lib/features/profile/widget/address/view model/address_notifier.dart
+++ lib/features/profile/widget/address/view model/address_notifier.dart
@@ -1,6 +1,19 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sadhana_cart/features/profile/widget/address/model/address_model.dart';
+import 'package:sadhana_cart/features/profile/widget/address/service/address_service.dart';
+
+final addressAsync = FutureProvider<List<AddressModel>>((ref) async {
+  return await AddressService.fetchAllAddress();
+});
 
 class AddressNotifier extends StateNotifier<List<AddressModel>> {
   AddressNotifier() : super([]);
+
+  void updateAddress() async {
+    state = await AddressService.fetchAllAddress();
+  }
+
+  void deleteAddress({required String id}) {
+    state = state.where((e) => e.id != id).toList();
+  }
 }
