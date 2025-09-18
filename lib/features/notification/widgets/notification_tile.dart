8607cbff1398b84fa8feb626587e23de12ff2dde
Sathish g <srisrisrisathish176@gmail.com>
2025-09-18 18:17:45 +0530

    updated notification service with local storage

--- lib/features/notification/widgets/notification_tile.dart
+++ lib/features/notification/widgets/notification_tile.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:sadhana_cart/core/colors/app_color.dart';
 import 'package:sadhana_cart/core/constants/app_images.dart';
+import 'package:sadhana_cart/core/helper/navigation_helper.dart';
 import 'package:sadhana_cart/features/notification/view%20model/notification_notifier.dart';
 
 class NotificationTile extends ConsumerWidget {
@@ -23,57 +24,70 @@ class NotificationTile extends ConsumerWidget {
         physics: const ClampingScrollPhysics(),
         itemBuilder: (context, index) {
           final notify = notification[index];
-          return Container(
-            margin: const EdgeInsets.all(8),
-            height: size.height * 0.1,
-            width: size.width * 1,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.shade300,
-                  blurRadius: 5,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Slidable(
-              closeOnScroll: true,
-              direction: Axis.horizontal,
-              endActionPane: ActionPane(
-                dragDismissible: true,
-                closeThreshold: 0.5,
-                motion: const BehindMotion(),
-                children: [
-                  SlidableAction(
-                    icon: Icons.delete,
-                    label: "Delete",
-                    backgroundColor: AppColor.onboardButtonColor,
-                    borderRadius: BorderRadius.circular(12),
-                    autoClose: true,
-                    onPressed: (context) {},
+          return GestureDetector(
+            onTap: () {
+              navigateWithRoute(context: context, screenPath: notify.screen);
+            },
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              height: size.height * 0.1,
+              width: size.width * 1,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade300,
+                    blurRadius: 5,
+                    offset: const Offset(0, 2),
                   ),
                 ],
               ),
-              child: ListTile(
-                title: Text(
-                  notify.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
+              child: Slidable(
+                closeOnScroll: true,
+                direction: Axis.horizontal,
+                endActionPane: ActionPane(
+                  dragDismissible: true,
+                  closeThreshold: 0.5,
+                  motion: const BehindMotion(),
+                  children: [
+                    SlidableAction(
+                      icon: Icons.delete,
+                      label: "Delete",
+                      backgroundColor: AppColor.onboardButtonColor,
+                      borderRadius: BorderRadius.circular(12),
+                      autoClose: true,
+                      onPressed: (context) async {
+                        ref
+                            .read(notificationProvider.notifier)
+                            .deleteNotification(index);
+                      },
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  title: Text(
+                    notify.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  subtitle: Text(
+                    notify.body,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                  leading: Image.asset(
+                    AppImages.appLogo,
+                    height: 40,
+                    width: 40,
                   ),
                 ),
-                subtitle: Text(
-                  notify.body,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(color: Colors.grey),
-                ),
-                leading: Image.asset(AppImages.appLogo, height: 40, width: 40),
               ),
             ),
           );
